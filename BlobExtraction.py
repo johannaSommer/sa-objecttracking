e504ef638d43f91399a1375a4c664ddbde4b9598
Johanna Sommer <johanna@mail-sommer.com>
2019-02-04 15:34:54 +0100

    refined blob extraction params, added basic plot

---
+++ BlobExtraction.py
@@ -0,0 +1,46 @@
+# Standard imports
+import cv2
+import numpy as np
+
+# Read image
+im = cv2.imread("Capture3.PNG")
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+filterByColor = False
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 40
+# params.maxArea = 200
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.5
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.8
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.4
+params.maxInertiaRatio = 0.5
+
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create(params)
+
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
+                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+print(keypoints[0].pt)
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
