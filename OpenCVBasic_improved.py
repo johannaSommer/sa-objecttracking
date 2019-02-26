6e2c464abd4444a998437b3579d11a070b28ec8f
johannaSommer <johanna@mail-sommer.com>
2019-02-26 09:39:37 +0100

    added first draft of blender script

--- OpenCVBasic_improved.py
+++ OpenCVBasic_improved.py
@@ -56,8 +56,6 @@ while True:
 
     keypoints = detector.detect(frameinverted)
 
-    # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
     im_with_keypoints = cv.drawKeypoints(frameinverted, keypoints, np.array([]), (0, 0, 255),
                                           cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv.imshow("Keypoints", im_with_keypoints)
