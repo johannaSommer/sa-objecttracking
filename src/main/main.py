dfb68517e43ba65d62906baa95943efd2f2bef56
johannaSommer <johanna@mail-sommer.com>
2019-03-17 20:05:07 +0100

    paper: refactored chapters, started backsub section

--- src/main/main.py
+++ src/main/main.py
@@ -6,8 +6,8 @@ BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_marc
 
 filelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
 
-for x in filelist:
-    Blobdetection(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex', x), False).showbdimg()
+#for x in filelist:
+#    Blobdetection(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex', x), False).showbdimg()
 
 #backsub = Backgroundsub(BASEPATH).savebgs()
 #blobdec = Blobdetection(BASEPATH, False).showbdimg()
