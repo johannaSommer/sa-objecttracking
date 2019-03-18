bb8879902b0a51b0108e51a92671a40351f5f24c
johannaSommer <johanna@mail-sommer.com>
2019-03-18 20:01:13 +0100

    added maybe final version of colorfilter

--- src/main/utils.py
+++ src/main/utils.py
@@ -1,14 +1,14 @@
 def islight(x, frame):
-    # submatrix = frame[int(x.pt[1] - x.size/4):int(x.pt[1] + x.size/4), int(x.pt[0]- x.size/4):int(x.pt[0]+x.size/4)]
-    # if submatrix.sum()/submatrix.size > 220:
-    #     print(submatrix.sum() / submatrix.size)
-    
     point1 = x.pt
     x1 = int(point1[0])
     y1 = int(point1[1])
     R = int(frame[y1, x1][1])
     B = int(frame[y1, x1][0])
     G = int(frame[y1, x1][2])
-    if (R+G+B)/3 > 220:
+    if (R+G+B)/3 > 230:
+        print('over: ' + str(R) + " " + str(G) + " " + str(B))
+        return True
+    if abs(R-B)<50 and abs(R-G)<50 and abs(G-B)<50 and R>120 and G>120 and B>120:
+        print(R, G, B)
         return True
     return False
