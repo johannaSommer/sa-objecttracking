b4caeb57a7b9e45d924cc5c13cec4fcdfc4bb5ff
johannaSommer <johanna@mail-sommer.com>
2019-03-19 09:56:35 +0100

    removed reviously made addition to colorfilter

--- src/main/backgroundsub.py
+++ src/main/backgroundsub.py
@@ -24,25 +24,13 @@ class Backgroundsub:
         self.detector = cv.SimpleBlobDetector_create(params)
 
     def applybgs(self):
-        #f = open("data.csv", "w")
-        #f.write("")
         while True:
             ret, frame = self.cap.read()
             if frame is None:
                 break
             fgmask = self.backsub.apply(frame)
             frameinverted = cv.bitwise_not(fgmask)
-            keypoints = self.detector.detect(frameinverted)
-            for x in keypoints:
-                if islight(x, frame) is False:
-                    keypoints.remove(x)
-            im_with_keypoints = cv.drawKeypoints(frameinverted, keypoints, np.array([]), (0, 0, 255),
-                                                 cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            f = open("data.csv", "a")
-            for x in keypoints:
-                f.write("-" + str(int(x.pt[1])) + " ; " + str(int(x.pt[0])) + " -- ")
-            f.write("\n")
-            cv.imshow("keypoints", im_with_keypoints)
+            cv.imshow("fgmask", frameinverted)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -51,7 +39,7 @@ class Backgroundsub:
         outfile = self.infile + "__" + str(time.time()) + ".wmv"
         video = cv.VideoWriter(outfile, cv.VideoWriter_fourcc('W', 'M', 'V', '1'), 30, fsize, False)
         if video.isOpened() is False:
-            print("nope")
+            print("no file opened")
         while True:
             ret, frame = self.cap.read()
             if frame is None:
@@ -59,7 +47,7 @@ class Backgroundsub:
             fgmask = self.backsub.apply(frame)
             frameinverted = cv.bitwise_not(fgmask)
             video.write(frameinverted)
-            cv.imshow("framinverted", frameinverted)
+            cv.imshow("frameinverted", frameinverted)
             if cv.waitKey(1) & 0xFF == ord('p'):
                 cv.imwrite(self.infile + "__" + str(time.time()) + ".jpg", frameinverted)
             if cv.waitKey(1) & 0xFF == ord('q'):
--- src/main/main.py
+++ src/main/main.py
@@ -2,7 +2,7 @@ from backgroundsub import Backgroundsub
 from blobdetection import Blobdetection
 import os
 
-BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'original', 'snip1_3.mp4')
+BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'backsubbed', 'snip1_3.mp4')
 
 filelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
 
