66a3400be4c4b2cf17f9ed48d6854b56c7b98baf
johannaSommer <johanna@mail-sommer.com>
2019-04-19 15:40:07 +0200

    synchronized video edit

--- src/main/main.py
+++ src/main/main.py
@@ -10,7 +10,7 @@ filelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), '
 #for x in filelist:
 #    Blobdetection(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex', x), False).showbdimg()
 
-#Backgroundsub(BASEPATH).applybgs()
+#Backgroundsub(BASEPATH).savebgs()
 blobdec = Blobdetection(BASEPATH, True).showbd()
 
 
