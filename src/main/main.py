b6442d75a916eb92598944912c6924200ce59652
johannaSommer <johanna@mail-sommer.com>
2019-04-21 09:56:51 +0200

    reorg

--- src/main/main.py
+++ src/main/main.py
@@ -2,7 +2,7 @@ from backgroundsub import Backgroundsub
 from blobdetection import Blobdetection
 import os
 
-BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'SYNC', 'sync_2_2bgs.wmv')
+BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'SYNC', 'sync_1_2bgs.wmv')
 
 filelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
 
