0d5aace267dd3b27dd7c7f0ff87449cfa75aaf72
johannaSommer <johanna@mail-sommer.com>
2019-04-24 13:04:00 +0200

    changed writing to data file, includes frame now

--- src/main/DataHandler.py
+++ src/main/DataHandler.py
@@ -4,17 +4,28 @@ import numpy as np
 class DataHandler:
     def writetocsv(self, trajectory, show):
         if show is True:
+            keypoints = []
+            for x in trajectory[0]:
+                keypoints.append(x[0])
             image = cv.imread("C:\Users\IBM_ADMIN\Desktop\GitHub\sa-objecttracking\src\snip2_0__1552679476.08.jpg")
-            im_with_traj = cv.drawKeypoints(image, trajectory[0], np.array([]), (0, 0, 255),
+            im_with_traj = cv.drawKeypoints(image, keypoints, np.array([]), (0, 0, 255),
                                             cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             while True:
                 cv.imshow("traj", im_with_traj)
                 if cv.waitKey(1) & 0xFF == ord('q'):
                     break
-        # write trajectory to csv here
+
         f = open("data.csv", "w")
         f.write("")
         f = open("data.csv", "a")
+        length = len(trajectory[0])
+        counter = 0
         for x in trajectory[0]:
-            f.write(str(int(x.pt[0])) + " ; " + str(-int(x.pt[1])))
-            f.write("\n")
+            counter += 1
+            if x[1] == counter:
+                f.write(str(int(x[0].pt[0])) + " ; " + str(-int(x[0].pt[1])))
+                f.write("\n")
+            else:
+                while counter < x[1]:
+                    f.write("\n")
+                    counter += 1
