a72389f8e9376f88e2949de55b202555d4c3645a
johannaSommer <johanna@mail-sommer.com>
2019-05-10 19:00:55 +0200

    corrected all backsub snips in sync

--- src/main/main.py
+++ src/main/main.py
@@ -3,17 +3,17 @@ from BlobDetection import Blobdetection
 from DataHandler import DataHandler
 import os
 
-BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'SYNC', 'backsub', 'sync_1_2bgs.wmv')
+BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sync_videos', 'snips', 'sync_2_6.mp4')
 BASEPATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'SYNC', 'backsub', 'sync_2_8bgs.wmv')
 
-filelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
+# qfilelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
 
 
 #for x in filelist:
 #Blobdetection(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex', x), False).showbdimg()
-#Backgroundsub(BASEPATH).savebgs()
-trajectory = Blobdetection(BASEPATH, True, False).showbd()
-DataHandler().writetocsv(trajectory, True)
+Backgroundsub(BASEPATH).savebgs()
+#trajectory = Blobdetection(BASEPATH, True, False).showbd()
+#DataHandler().writetocsv(trajectory, True)
 #trajectory2 = Blobdetection(BASEPATH2, True, False).showbd()
 #DataHandler().adddimension(trajectory2)
 
