52ebe13b04b2fb47529c471f5b917e9d131fbb4a
johannaSommer <johanna@mail-sommer.com>
2019-05-15 10:17:44 +0200

    added blobdec test suite

--- src/main/main.py
+++ src/main/main.py
@@ -5,7 +5,7 @@ from datahandling import redim
 from datahandling import datacleanse
 import os
 
-BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sync_videos', 'backsub', 'sync_2_5bgs.wmv')
+BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sync_videos', 'snips', 'sync_2_7.mp4')
 # BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),  'test.png')
 
 # qfilelist = os.listdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos_march', 'snips', 'framex'))
@@ -13,9 +13,9 @@ BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sync_videos
 
 #for x in filelist:
 # Blobdetection(BASEPATH, False).showbdimg()
-# Backgroundsub(BASEPATH).savebgs()
-trajectory = Blobdetection(BASEPATH, True).showbd(False)
-writetocsv(trajectory, 'test.csv')
+Backgroundsub(BASEPATH).savebgs()
+#trajectory = Blobdetection(BASEPATH, True).showbd(False)
+#writetocsv(trajectory, 'test.csv')
 #trajectory2 = Blobdetection(BASEPATH2, True, False).showbd()
 #DataHandler().adddimension(trajectory2)
 #redim(BASEPATH)
