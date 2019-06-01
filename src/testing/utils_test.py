866dc7aa41a22f0f066aea107913f0b1fa6f58f1
johannaSommer <johanna@mail-sommer.com>
2019-06-01 19:17:24 +0200

    added distance test

--- src/testing/utils_test.py
+++ src/testing/utils_test.py
@@ -1,4 +1,5 @@
 from main.utils import match_traj
+from main.utils import distance
 import cv2 as cv
 
 
@@ -13,8 +14,6 @@ def test_trajmatch():
           [cv.KeyPoint(10, 9, 10,  _angle=-1,  _response=0,  _octave=0,  _class_id=-1), 2]], 2]
     ]
     ret = match_traj(keypoint, list, threshold, frame_num, rec)
-    print(ret[0])
-    print[ret[1]]
     parsed = [
         [[ret[0][0][0][0].pt[0], ret[0][0][0][0].pt[1]], [ret[0][0][1][0].pt[0], ret[0][0][1][0].pt[1]]],
         [[ret[1][0][0][0].pt[0], ret[1][0][0][0].pt[1]], [ret[1][0][1][0].pt[0], ret[1][0][1][0].pt[1]]]
@@ -24,3 +23,10 @@ def test_trajmatch():
         [[10.0, 10.0], [10.0, 10.0]]
     ]
     assert parsed == expected
+
+
+def test_distance():
+    keypoint = cv.KeyPoint(1, 1, 1,  _angle=-1,  _response=0,  _octave=0,  _class_id=-1)
+    trajectorykp = cv.KeyPoint(2, 2, 2,  _angle=-1,  _response=0,  _octave=0,  _class_id=-1)
+    expected = 1.508
+    assert expected == round(distance(keypoint, trajectorykp), 3)
