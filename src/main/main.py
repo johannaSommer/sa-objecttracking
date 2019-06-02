83472c47a839bd009d18110084f58d3963626f20
johannaSommer <johanna@mail-sommer.com>
2019-06-02 14:00:01 +0200

    created sequential main file

--- src/main/main.py
+++ src/main/main.py
@@ -7,8 +7,42 @@ from analysis import categorize
 from analysis import determine_speed
 import os
 
-BASEPATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos', 'data', 'sync_4.csv')
-BASEPATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'videos', 'data', 'sync_4_og.csv')
+PATH1 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_1_1.mp4')
+PATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_2_1.mp4')
 
-categorize(BASEPATH)
-determine_speed(BASEPATH2, BASEPATH)
+PATH3 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_1_1bgs.wmv')
+PATH4 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_2_1bgs.wmv')
+
+PATH5 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_1_og.csv')
+PATH6 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sampledata', 'sync_1_norm.csv')
+
+''' 
+Der folgende Code wurde so zusammengestellt, dass einfach die einzelnen Schritte des Programms nachvollzogen werden 
+koennen. Fuer die entsprechenden Schritte koennen einfach die zugehoerigen Code-Segmente auskommentiert und
+ausgefuerht werden
+'''
+
+# Background Subtraction auf beide Snippets anwenden und Output speichern
+# bgs1 = Backgroundsub(PATH1)
+# bgs1.savebgs()
+# bgs2 = Backgroundsub(PATH2)
+# bgs2.savebgs()
+
+# Blob Detection auf beide BackSub Videos anwenden
+# bd1 = Blobdetection(PATH3, True, True)
+# trajectory1 = bd1.showbd()
+# bd2 = Blobdetection(PATH4, True, False)
+# trajectory2 = bd2.showbd()
+
+# horizontale Flugbahn in CSV-Datei schreiben
+# writetocsv(trajectory1, 'test.csv')
+# vertikale Flugbahn kann nur von Hand bestimmt werden
+# print(trajectory2)
+
+# leere Frames in CSV ausfuellen
+# datacleanse(PATH5)
+# redim(PATH5)
+
+# Klassifikation und Geschwindigkeit hinzufuegen
+# categorize(PATH6)
+# determine_speed(PATH5, PATH6)
